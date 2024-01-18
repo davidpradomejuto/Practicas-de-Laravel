b2ae521e5871ce85a30dd7a2a0cd8fd89168ea38
DavidPrado <david.pradomejuto@iesmiguelherrero.com>
2024-01-18 15:57:45 +0100

    update

--- resources/views/animales/index.blade.php
+++ resources/views/animales/index.blade.php
@@ -6,15 +6,15 @@
 <div class="flex flex-row gap-5 flex-wrap justify-center mx-5 my-2">
 @foreach ($animales as $animal)
 
-    <a class="max-w-sm rounded overflow-hidden shadow-lg" href='{{ route('animales.show',$animal['especie'])}}'>
-        <img class="" width="300px" height="150px" src='{{asset("assets/imagenes/$animal[imagen]")}}' alt="Imagen del animal">
+    <a class="max-w-sm rounded overflow-hidden shadow-lg" href='{{ route('animales.show',$animal->especie)}}'>
+        <img class="" width="300px" height="150px" src='{{asset("assets/imagenes/$animal->imagen")}}' alt="Imagen del animal">
         <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">{{ $animal['especie'] }}</div>
+          <div class="font-bold text-xl mb-2">{{ $animal->especie }}</div>
           <div class="text-gray-700 text-base">
-            <p>Peso: {{ $animal['peso'] }}</p>
-            <p>Altura: {{ $animal['altura']}}</p>
-            <p>Fecha de nacimiento: {{ $animal['fechaNacimiento'] }}</p>
-            <p>Alimentacion: {{ $animal['alimentacion'] }}</p>
+            <p>Peso: {{ $animal->peso }}</p>
+            <p>Altura: {{ $animal->altura}}</p>
+            <p>Fecha de nacimiento: {{ $animal->fechaNacimiento }}</p>
+            <p>Alimentacion: {{ $animal->alimentacion }}</p>
           </div>
         </div>
 
